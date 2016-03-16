aa9c25bcd0745217e00b04731e2005676d856fb0
quteron <quteron@gmail.com>
2016-03-16 16:35:20 -0400

    Fixed implementation

--- algorithms/sorting/comparison-sort/insertion-sort/insertion-sort.js
+++ algorithms/sorting/comparison-sort/insertion-sort/insertion-sort.js
@@ -18,56 +18,93 @@
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  */
-
-/**
- * Swaps two elements in an array by their indexes.
- * @param {Array} array original array
- * @param {int} i index of the first element to swap
- * @param {int} j index of the second element to swap
- * @return {void}
- */
-function swap(array, i, j) {
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-}
-
-/**
- * Checks whether first passed element is less than second passed element.
- * @param array original array
- * @param i index of the first element
- * @param j index of the second element
- * @returns {boolean} if true if first element is less than second, otherwise - false.
- */
-function less(array, i, j) {
-    return array[i] < array[j];
-}
-
-/**
- * Sorts in-place an array using insertion sort algorithm.
- * @param {Array} array original array to be sort
- * @param lo index of the first element to be sorted
- * @param hi index of the last element to be sorted
- * @return {Array} sorted array
- */
-function sort(array, lo, hi) {
-    var i, j;
-
-    if (lo === undefined) {
-        lo = 0;
+(function() {
+    /**
+     * Moves element by its index to one position to the right.
+     * @param {Array} array original array
+     * @param {int} i index of the first element to swap
+     * @param {int} j index of the second element to swap
+     * @return {void}
+     */
+    function shiftRight(array, i) {
+        array[i + 1] = array[i];
+    }
+    
+    /**
+     * Checks whether first passed element is less than second passed element.
+     * @param {Array} array original array
+     * @param {int} a first element to compare
+     * @param {int} b second element to compare
+     * @returns {boolean} if true if first element is less than second, otherwise - false.
+     */
+    function less(a, b) {
+        return a < b;
     }
 
-    if (hi === undefined) {
-        hi = array.length - 1;
-    }
-
-    for (i = lo + 1; i <= hi; i++) {
-        j = i;
-        while (j > 0 && less(array, j, j - 1)) {
-            swap(array, j, j - 1);
-            j--;
+    /**
+     * Checks whether passed array is sorted.
+     * @param {Array} array to be cheked
+     * @param {int} lo index of the first element to be sorted
+     * @param {int} hi index of the last element to be sorted
+     * @returns {boolean} if true if array is sorted, otherwise - false.
+     */
+    function sorted(array, lo, hi) {
+        var n = array.length, 
+            i;
+    
+        if (lo === undefined) {
+            lo = 0;
         }
+    
+        if (hi === undefined) {
+            hi = n - 1;
+        }
+        
+        for (i = 1; i <= n; i++) {
+            if (less(array[i], array[i - 1])) {
+                return false;
+            }
+        }
+    
+        return true;
     }
-
-    return array;
-}
+    /**
+     * Sorts in-place an array using insertion sort algorithm.
+     * @param {Array} array original array to be sort
+     * @param {int} lo index of the first element to be sorted
+     * @param {int} hi index of the last element to be sorted
+     * @return {Array} sorted array
+     */
+    function sort(array, lo, hi) {
+        var i, j, temp;
+    
+        if (lo === undefined) {
+            lo = 0;
+        }
+    
+        if (hi === undefined) {
+            hi = array.length - 1;
+        }
+    
+        for (i = lo + 1; i <= hi; i++) {
+            temp = array[i];
+            
+            for(j = i - 1; j >= lo && less(temp, array[j]); j--) {
+                shiftRight(array, j);
+            }
+            
+            array[j + 1] = temp;
+        }
+    
+        return array;
+    }
+    
+    var a1 = [5, 2, 1, 3, 4];
+    sort(a1);
+    console.log(sorted(a1));
+    console.log(a1); // [1, 2, 3, 4, 5]
+    
+    var a2 = [5, 2, 1, 3, 4];
+    sort(a2, 1, 3);
+    console.log(a2); // [5, 1, 2, 3, 4]
+}());
